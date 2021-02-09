a4afeaba03757a72f4b9d6a95087a87a50be95d1
Rod Hilton <rod.hilton@gmail.com>
2021-02-08 22:57:14 -0700

    Made it easier to add other sites
    This adds a little duplication but that's to make it less specific to
    google sites only. There's now a single array to add lines to in order
    to add custom sites. I've found this plugin very useful for internal
    work wikis, JIRA, code review links, source code browsers, etc.

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
-var docId = document.URL.match(/\/d\/([_A-Za-z0-9-]+)/)[1];
-var docHash = sha1(docId);
-var bgColor;
-switch (document.URL.match(/docs.google.com\/(\w+)/)[1]) {
-	case 'document': bgColor = [81, 142, 245, 255]; break;
-	case 'drawings': bgColor = [222, 83, 71, 255]; break;
-	case 'forms': bgColor = [114, 73, 188, 255]; break;
-	case 'presentation': bgColor = [245, 186, 19, 255]; break;
-	case 'spreadsheets': bgColor = [35, 165, 102, 255];
+var prefixesToColor = [
+  /*
+    Format here is three elements:
+    First, a regex which contains a capturing group for the "document id" (the thing you want to hash for uniqueness)
+    Second, the foreground color, as an RGBA array
+    Third, the background color, as an RGBA array
+  */
+  [/docs.google.com\/document\/d\/([_A-Za-z0-9-]+)\//, [255, 255, 255, 255], [81, 142, 245, 255]],
+  [/docs.google.com\/drawings\/d\/([_A-Za-z0-9-]+)\//, [255, 255, 255, 255], [222, 83, 71, 255]],
+  [/docs.google.com\/forms\/d\/([_A-Za-z0-9-]+)\//, [255, 255, 255, 255], [114, 73, 188, 255]],
+  [/docs.google.com\/presentation\/d\/([_A-Za-z0-9-]+)\//, [255, 255, 255, 255], [245, 186, 19, 255]],
+  [/docs.google.com\/spreadsheets\/d\/([_A-Za-z0-9-]+)\//, [255, 255, 255, 255], [35, 165, 102, 255]],
+];
+
+for(var prefix of prefixesToColor) {
+  let m = document.URL.match(prefix[0]);
+  if(m) {
+    docId = m[1];
+    let docHash = sha1(docId);
+    let fgColor = prefix[1];
+    let bgColor = prefix[2];
+    let iconBase64 = new Identicon(docHash, { background: bgColor, foreground: fgColor }).toString();
+    let shortcutIcons = document.querySelectorAll('link[rel*="icon"]')
+    for (var i = 0; i < shortcutIcons.length; i++) {
+      shortcutIcons[i].href = 'data:image/png;base64,' + iconBase64;
+    }
+    break;
+  }
 }
-var iconBase64 = new Identicon(docHash, { background: bgColor, foreground: [255, 255, 255, 255] }).toString();
-document.querySelector('link[rel*="icon"]').href = 'data:image/png;base64,' + iconBase64;
